f14b333484389f3b4ad4fc4d3a6a1d71375faa87
ohsnappityph <60123280+ohsnappityph@users.noreply.github.com>
2020-06-17 14:27:09 +0800

    edited default and added pages

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,117 +1,133 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
       app
-    >
-      <v-list>
+      floating
+      mini-variant
+      expand-on-hover
+      color="grey darken-4"
+      >
+      <v-list dense>
         <v-list-item
-          v-for="(item, i) in items"
+          v-for="(item, i) in user"
           :key="i"
-          :to="item.to"
-          router
-          exact
+          :to="{name: item.route}"
+          link
         >
           <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon v-text="item.icon"></v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-item
+          v-for="(item, i) in pages"
+          :key="i"
+          :to="{name: item.route}"
+          link
+        >
+          <v-list-item-action>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+    <v-app-bar app color="#4A148C">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase">
+        <nuxt-link :to="{name: 'dashboard'}" style="text-decoration: none; color: white;">
+          <span class="font-weight-bold">FPV</span>
+        </nuxt-link>
+      </v-toolbar-title>
     </v-app-bar>
+
     <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
+      <div>
+        <v-container fluid>
+          <nuxt />
+        </v-container>
+      </div>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+
+    <v-footer app>
+      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
+      <v-spacer></v-spacer>
+      <span>Created by OhSnappityPH</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
+  data: () => ({
+    drawer:false,
+    mini: true,
+    user: [
+      {
+        icon: 'mdi-account-box',
+        title: 'User',
+        route: 'user'
+      },
+    ],
+    pages: [
+      {
+        icon: 'mdi-view-dashboard',
+        title: 'Dashboard',
+        route: 'dashboard'
+      },
+      {
+        icon: 'mdi-calendar-month',
+        title: 'Scheduling',
+        route: 'scheduling'
+      },
+      {
+        icon: 'mdi-account',
+        title: 'Clients',
+        route: 'clients'
+      },
+      {
+        icon: 'mdi-account-hard-hat',
+        title: 'Surveyors',
+        route: 'surveyors'
+      },
+      {
+        icon: 'mdi-account-cash',
+        title: 'Payrolls',
+        route: 'payrolls'
+      },
+      {
+        icon: 'mdi-chart-bar',
+        title: 'Leads',
+        route: 'leads'
+      },
+      {
+        icon: 'mdi-view-dashboard-variant',
+        title: 'Projects',
+        route: 'projects'
+      },
+      {
+        icon: 'mdi-email-multiple',
+        title: 'Emailing',
+        route: 'emailing'
+      },
+      {
+        icon: 'mdi-chart-areaspline',
+        title: 'Reporting',
+        route: 'reporting'
+      },
+    ]
+  }),
 }
 </script>
+
+<style>
+
+</style>
